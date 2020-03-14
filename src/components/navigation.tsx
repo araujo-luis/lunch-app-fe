db0d6ea2d2e997878c2f36d84871db5f7196ef39
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-03-13 20:43:56 -0600

    Login

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,16 +1,22 @@
 import React, { FC } from 'react';
 import { Button, Navbar, Nav } from 'react-bootstrap'
-const Navigation: FC = () => {
+import { useAuth0 } from '../context/Auth0Context';
 
+const Navigation: FC = () => {
+    const { isAuthenticated, loginWithRedirect } = useAuth0();
     return (
         <Navbar bg="light" variant="light">
             <Navbar.Brand href="#home">Lunch Picker</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
             </Nav>
-            <Button>
-                Log in
-              </Button>
+            {!isAuthenticated && (
+
+                <Button onClick={() => loginWithRedirect({})}>
+                    Log in
+                </Button>
+
+            )}
         </Navbar>
     )
 }
