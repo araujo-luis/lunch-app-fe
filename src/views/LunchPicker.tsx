8ca966ee97a4abdeb89aeef034b579660ee545cf
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-03-15 22:08:23 -0600

    Call api

--- src/views/LunchPicker.tsx
+++ src/views/LunchPicker.tsx
@@ -1,32 +1,34 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import axios from 'axios';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const baseUrl = process.env.BASE_URL;
+const baseUrl = process.env.BASE_URL || 'http://localhost:4000';
 
 const LunchPicker: FC = () => {
 
-    const [randomLunch, setRandomLunch] = useState();
-    const [loading, setLoading] = useState();
-    const [errorCall, setErrorCall] = useState(false);
+    const [randomLunch, setRandomLunch] = useState({ data: null });
 
     const callApi = async () => {
-        try {
-            const data = await axios.get(`${baseUrl}/lunch`);
-            console.log("DATA");
-            console.log(data);
-            return data;
-        } catch (err) {
+        console.log("base URL");
+        console.log(baseUrl);
+        axios.get(`${baseUrl}/lunch`).then(res => {
+            console.log(res.data);
 
-        }
+            setRandomLunch({ data: res.data });
+
+        });
+
     }
+    useEffect(() => {
+        callApi();
+    }, []);
 
 
     return (
         <div>
-            <span>    {callApi()}</span>
+            <span> {randomLunch.data}</span>
             < h3 > Lunch Picker</h3 >
         </div>
     )
