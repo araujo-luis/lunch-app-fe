be492c6226a236f2923affc24b7c86fa94637acb
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-03-17 20:42:56 -0600

    Validation process

--- src/views/LunchForm.tsx
+++ src/views/LunchForm.tsx
@@ -1,42 +1,81 @@
-import React, { FC } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
 import { Form, Button, Col, Container, Row } from 'react-bootstrap';
 
 const LunchForm: FC = () => {
+
+    const [formValidated, setFormValidated] = useState(false);
+
+    const formHandler = (e: any) => {
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        console.log("VALIDATION");
+
+        setFormValidated(true);
+    };
+
     return (
         <Container>
             <Row className="justify-content-md-center">
                 <Col xs lg="12">
                     <h1>Create Lunch</h1>
 
-                    <Form>
+                    <Form noValidate validated={formValidated}  onSubmit={formHandler}>
                         <Form.Group controlId="plateName">
                             <Form.Label>Plate Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Plate Name" />
+                            <Form.Control required type="text" placeholder="Enter Plate Name" />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a Name.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="plateDescription">
                             <Form.Label>Plate Description</Form.Label>
-                            <Form.Control as="textarea" rows="2" />
+                            <Form.Control required as="textarea" rows="2" />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a Description.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="restaurantName">
                             <Form.Label>Restaurant Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Restaurant Name" />
+                            <Form.Control required type="text" placeholder="Enter Restaurant Name" />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a Restaurant.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="category">
                             <Form.Label>Category</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Category" />
+                            <Form.Control required type="text" placeholder="Enter Category" />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a Category.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="price">
                             <Form.Label>Price</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Price" />
+                            <Form.Control required type="text" placeholder="Enter Price" />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a Price.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="imageUrl">
                             <Form.Label>Plate Image URL</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Plate Image URL" />
+                            <Form.Control required type="text" placeholder="Enter Plate Image URL" />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a Image.
+                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Button variant="primary" type="submit">
